cd7fd819857a062e45c229519dcb989171bfcf00
Carol Borges <cboliveirasbr@gmail.com>
2023-07-12 18:11:29 -0300

    refactor: changing script to insert all time data at once

--- transaction_generator.sh
+++ transaction_generator.sh
@@ -1,33 +1,39 @@
-#!/bin/bash
+hour=0
+minute=0
 
-while true; do
-  time=$(date +%Hh%M)
-  approved=$((RANDOM % 10))
-  denied=$((RANDOM % 5))
-  refunded=$((RANDOM % 10))
-  reversed=$((RANDOM % 10))
-  backend_reversed=$((RANDOM % 10))
-  failed=$((RANDOM % 5))
-  processing=$((RANDOM % 8))
+while [ $hour -lt 24 ]; do
+  while [ $minute -lt 60 ]; do
+    time=$(printf "%02dh%02d" "$hour" "$minute")
+    approved=$((RANDOM % 10))
+    denied=$((RANDOM % 10 + 1))
+    refunded=$((RANDOM % 10))
+    reversed=$((RANDOM % 10 + 1))
+    backend_reversed=$((RANDOM % 10))
+    failed=$((RANDOM % 10 + 1))
+    processing=$((RANDOM % 8))
 
-  # Construct the JSON payload
-  payload='{
-    "time": "'"$time"'",
-    "data": {
-      "approved": '"$approved"',
-      "denied": '"$denied"',
-      "refunded": '"$refunded"',
-      "reversed": '"$reversed"',
-      "backend_reversed": '"$backend_reversed"',
-      "failed": '"$failed"',
-      "processing": '"$processing"'
-    }
-  }'
+    # Construct the JSON payload
+    payload='{
+      "time": "'"$time"'",
+      "data": {
+        "approved": '"$approved"',
+        "denied": '"$denied"',
+        "refunded": '"$refunded"',
+        "reversed": '"$reversed"',
+        "backend_reversed": '"$backend_reversed"',
+        "failed": '"$failed"',
+        "processing": '"$processing"'
+      }
+    }'
 
-  # Make the curl request with the JSON payload
-  curl -X POST http://localhost:3000/new_transaction \
-    -H "Content-Type: application/json" \
-    -d "$payload"
+    # Make the curl request with the JSON payload
+    curl -X POST http://localhost:3000/new_transaction \
+      -H "Content-Type: application/json" \
+      -d "$payload"
 
-  sleep 60  # Wait for 1 minute before sending the next request
+    minute=$((minute + 1))
+  done
+
+  minute=0
+  hour=$((hour + 1))
 done
